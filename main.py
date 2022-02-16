0102dd1df9aa8b4c2b863a075cde981d4eb044ea
Ossian <Ossianth@gmail.com>
2022-02-16 14:55:02 +0100

    More content

--- main.py
+++ main.py
@@ -2,10 +2,37 @@ import resources
 from resources import Character, Goblin
 from random import randint, shuffle
 
+
+
+
+def fight(players: list, enemies: list):
+    participants = players+enemies
+    shuffle(participants)
+    for char in participants:
+        target = ""
+        # Is the character a player or an enemy
+        if char in players:
+            target = choice(enemies)
+        else:
+            target = choice(players)
+        target.take_damage(char.get_attack())
+        if target.get_hp == 0:
+            print(f"{target.get_name()} has died.")
+            if type(target) == Goblin:
+                enemies.remove(target)
+            else:
+                players.remove(target)
+            participants.remove(target)
+        else:
+            print(f"{target.get_name()} has {target.get_health} hp left.")
+        if len(players) == 0 or len(enemies) == 0:
+            break
+
+
 if __name__ == "__main__":
     nemy = Character("Nemy", 20, 5, 2)
     goblin_one = Goblin(10, 3, 1)
-    
+    players.append(nemy)
     print(nemy)
     print()
     print(goblin_one)
